45247591ec3bad855eeffcf8670329dc38e8d505
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-09-29 18:38:37 +0530

    changes

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-backend-xqn1.onrender.com");
+const socket = io(
+  "http://ec2-52-66-251-149.ap-south-1.compute.amazonaws.com:8000/"
+);
 
 export default socket;
